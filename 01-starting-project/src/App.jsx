cc92f07cbc3da9968d21f259211c18f02fd47129
ryanboodhoo <boodho46@gmail.com>
2025-03-14 10:00:05 -0400

    fixed the bug

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,37 +1,34 @@
+import { useState } from "react";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
-import { useState } from "react";
-function App() {
 
-  const [projectsState,] = useState({
-    selectedProjectedId:undefined,
-    projects : []
+function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectedId: undefined,
+    projects: null,
   });
 
-  function handleStartAddProject(){
-    setProjectState(prevState => {
-      return{
-        ...prevState,
-        selectedProjectedId :null,
-        
-      };
-    });
+  function handleStartAddProject() {
+    setProjectState((prevState) => ({
+      ...prevState,
+      selectedProjectedId: null,
+    }));
   }
 
   let content;
 
-  if (projectsState.selectedProjectedId === null){
-    content = <NewProject/>
-  }else if (projectsState.selectedProjectedId === undefined){
-    content = <NoProjectSelected onStartAddProject = {handleStartAddProject} />
+  if (projectsState.selectedProjectedId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectedId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-        <ProjectsSidebar onStartAddProject = {handleStartAddProject}/>
-     {content}
-     </main>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
   );
 }
 
