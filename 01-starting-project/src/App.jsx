3a2efb274076d0d822df6fa78520b0eae50f0d9f
ryanboodhoo <boodho46@gmail.com>
2025-03-14 17:02:23 -0400

    159

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,14 +5,14 @@ import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   const [projectsState, setProjectState] = useState({
-    selectedProjectId: undefined,  // Fixed typo here
+    selectedProjectId: undefined,  
     projects: [],
   });
 
   function handleStartAddProject() {
     setProjectState((prevState) => ({
       ...prevState,
-      selectedProjectId: null,  // Fixed typo here
+      selectedProjectId: null,  
     }));
   }
 
@@ -25,19 +25,19 @@ function App() {
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
-        selectedProjectId: newProject.id, // Auto-select new project after adding
+        selectedProjectId: newProject.id, 
       };
     });
   }
 
-  console.log(projectsState);
-
+ 
   let content;
 
-  if (projectsState.selectedProjectId === null) {  // Fixed typo here
+  if (projectsState.selectedProjectId === null) { 
     content = <NewProject onAdd={handleAddProject} />;
-  } else if (projectsState.selectedProjectId === undefined) {  // Fixed typo here
+  } else if (projectsState.selectedProjectId === undefined) {  
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
