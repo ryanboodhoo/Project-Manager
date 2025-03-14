9284c0f1c1d642bff1582478ace39ce437e361ff
ryanboodhoo <boodho46@gmail.com>
2025-03-14 17:04:42 -0400

    done up to chapter 9

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,49 +1,82 @@
-import { useState } from "react";
-import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
-import NewProject from "./components/NewProject.jsx";
-import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from 'react';
+
+import NewProject from './components/NewProject.jsx';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
-  const [projectsState, setProjectState] = useState({
-    selectedProjectId: undefined,  
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
     projects: [],
   });
 
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
   function handleStartAddProject() {
-    setProjectState((prevState) => ({
-      ...prevState,
-      selectedProjectId: null,  
-    }));
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
   }
 
   function handleAddProject(projectData) {
-    setProjectState((prevState) => {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
         selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
-        selectedProjectId: newProject.id, 
       };
     });
   }
 
- 
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
   let content;
 
-  if (projectsState.selectedProjectId === null) { 
-    content = <NewProject onAdd={handleAddProject} />;
-  } else if (projectsState.selectedProjectId === undefined) {  
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else {
+    content = <SelectedProject project={selectedProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
--- 01-starting-project/src/components/NoProjectSelected.jsx
+++ 01-starting-project/src/components/NoProjectSelected.jsx
@@ -1,23 +1,72 @@
-import Button from './Button.jsx';
-import noProjectImage from '../assets/no-projects.png';
+import { useRef } from 'react';
 
-export default function NoProjectSelected({ onStartAddProject }) {
+import Input from './Input.jsx';
+import Modal from './Modal.jsx';
+
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
-    <div className="mt-24 text-center w-2/3">
-      <img
-        src={noProjectImage}
-        alt="An empty task list"
-        className="w-16 h-16 object-contain mx-auto"
-      />
-      <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">
-        No Project Selected
-      </h2>
-      <p className="text-stone-400 mb-4">
-        Please select a project or get started with a new one.
-      </p>
-      <p className="mt-8">
-        <Button onClick={onStartAddProject}>Create a new project</Button>
-      </p>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
+      </div>
+    </>
   );
 }
